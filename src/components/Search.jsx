1de1166c054af7dce2532c5f0e678b1cde1c37ce
Namima Islam <namimaislam308@gmail.com>
2024-10-18 12:06:04 -0400

    new movie

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// function Search({ searchTerm, onSearchChange }) {
+//     return(
+//     <div>
+//       <input 
+//       type="text"
+//       placeholder="Movie Search..."
+//       value={searchTerm}
+
+//     </div>
+//     );
+// }
+
+
+
+// export default Search;
